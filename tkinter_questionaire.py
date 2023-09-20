fb9ad2ba06e762f192a267d657d6dfaee1fe8dba
uklewis124 <53154528+uklewis124@users.noreply.github.com>
2023-09-20 13:43:34 +0000

    aaa

--- tkinter_questionaire.py
+++ tkinter_questionaire.py
@@ -4,19 +4,19 @@ def main():
     rng = rng_module.OscarRNG()
     while True:
         dice_type = input("Enter dice type (D4, D6, D12, D20): ")
-        if dice_type.lower() == "D4":
+        if dice_type.upper() == "D4":
             dice_total = rng.next(1,5)
             print(f"D4 {dice_total}")
             continue
-        elif dice_type == "D6":
+        elif dice_type.upper() == "D6":
             dice_total = rng.next(1,7)
             print(f"D6 {dice_total}")
             continue
-        elif dice_type == "D12":
+        elif dice_type.upper() == "D12":
             dice_total = rng.next(1,13)
             print(f"D12 {dice_total}")
             continue
-        elif dice_type == "D20":
+        elif dice_type.upper() == "D20":
             dice_total = rng.next(1,21)
             print(f"D20 {dice_total}")
             continue
