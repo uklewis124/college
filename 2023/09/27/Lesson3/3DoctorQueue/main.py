8e949ea35c804cdbae8e4b93edb610aff1e76c40
uklewis124 <53154528+uklewis124@users.noreply.github.com>
2023-09-27 13:45:50 +0000

    fdsgafdfa

--- 2023/09/27/Lesson3/3DoctorQueue/main.py
+++ 2023/09/27/Lesson3/3DoctorQueue/main.py
@@ -1,6 +1,17 @@
 import time
-
-# Pre-Requisites:
 import resources
 
-resources.test()
+# Pre-Requisites:
+resources.load() # Testing the resources plugin to see if its working
+
+patients = {
+    0 : {"Name": "Jimmy", "Priority": False, "Doctor": "Dr. Smith"},
+    1 : {"Name": "Sarah", "Priority": True, "Doctor": "Dr. Smith"},
+    2 : {"Name": "Bob", "Priority": False, "Doctor": "Dr. Joe"},
+    3 : {"Name": "Dan", "Priority": False, "Doctor": "Dr. Joe"},
+}
+
+resources.test() # Asking user whether they are testing the program or not
+
+
+
