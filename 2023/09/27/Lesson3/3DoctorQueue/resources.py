6704ea9559070ba140e50f558cbb4090bfb35d63
uklewis124 <53154528+uklewis124@users.noreply.github.com>
2023-09-27 13:00:43 +0000

    45twsh

--- 2023/09/27/Lesson3/3DoctorQueue/resources.py
+++ 2023/09/27/Lesson3/3DoctorQueue/resources.py
@@ -1,2 +1,20 @@
-def test():
-    print("Hello, world!")
+def load():
+    print("Resources plugin loaded")
+
+def main():
+    print("You are running this module directly, Try using the main program instead...")
+
+
+
+
+
+
+
+
+
+
+
+
+## DO NOT EDIT BELOW THIS LINE
+if __name__ == "__main__":
+    main()
