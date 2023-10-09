279e36c95c1d3ddeb2508ee961b204912a16ecc4
uklewis124 <53154528+uklewis124@users.noreply.github.com>
2023-10-09 08:38:02 +0000

    added stuff

--- 2023/10/05/pandas_cars_program.py
+++ 2023/10/05/pandas_cars_program.py
@@ -1,6 +1,5 @@
 '''summary goes here'''
 import pandas as pd
-import numpy as np
 import os
 
 
@@ -9,11 +8,18 @@ RESOURCES = {
     "https://sparkbyexamples.com/pandas/pandas-get-column-names/#:~:text=You%20can%20get%20the%20column,it%20using%20print()%20statement."
 }
 df = pd.read_csv(PATH, sep=';')
-os.system('cls')
+os.system('clear') # Works on bash, CLS for windows.
 
+'''Grabs the names of all columns and prints each column number name.'''
 def get_names(dataframe):
     column_headers = dataframe.columns.values.tolist()
     for i in range(0,len(column_headers)):
         print(f"The name of column number {i+1} is: {column_headers[i]}")
 
 get_names(df)
+
+'''Finding the car with the highest MPG'''
+def highest_mpg(dataframe):
+    highest = dataframe.idxmax("MPG")
+    print(highest)
+    return highest
