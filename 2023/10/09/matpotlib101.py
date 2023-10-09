7f3b8e09d5f5aa25f11b017829857df4306b6546
uklewis124 <53154528+uklewis124@users.noreply.github.com>
2023-10-09 08:38:00 +0000

    added stuff

---
+++ 2023/10/09/matpotlib101.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Preparing data
+dataframe = pd.read_csv("2023/10/09/Student Performance.csv")
+# Defining columns
+
+
+# Do not delete this line
+plt.savefig("2023/10/09/matplotlib_output.png")
